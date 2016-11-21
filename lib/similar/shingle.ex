6f32632b6bd0cfa8198cb732e8fa214b0835abdd
pragdave <dave@pragdave.me>
2016-11-21 11:20:32 -0600

    use Task.async_stream

--- lib/similar/shingle.ex
+++ lib/similar/shingle.ex
@@ -8,10 +8,10 @@ defmodule Similar.Shingle do
   end
 
   def lists_of_ngrams(named_strings, ngram_size) do
-    Enum.map(named_strings, fn [name, string] ->
-      Task.async(fn -> {name, ngrams_in(string, ngram_size)} end)
+    Task.async_stream(named_strings, fn [name, string] ->
+      {name, ngrams_in(string, ngram_size)}
     end)
-    |> Enum.map(&Task.await/1)
+    |> Enum.map(fn {:ok, result} -> result end)
   end
 
   def score_cross_product(lists_of_ngrams) do
