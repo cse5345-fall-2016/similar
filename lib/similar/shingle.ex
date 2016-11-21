a9e05f7768ebce60e956c63d6512525f5cd6a2d0
pragdave <dave@pragdave.me>
2016-11-20 21:25:10 -0600

    refactor and tidy

--- lib/similar/shingle.ex
+++ lib/similar/shingle.ex
@@ -3,16 +3,23 @@ defmodule Similar.Shingle do
   import Similar.Ngrams, only: [ ngrams_in: 2 ]
 
   def similarity(named_strings, ngram_size) do
-    ngrams_for_strings =
+    lists_of_ngrams(named_strings, ngram_size)
+    |> score_cross_product
+  end
+
+  def lists_of_ngrams(named_strings, ngram_size) do
       Enum.map(named_strings, fn [name, string] ->
         Task.async(fn -> {name, ngrams_in(string, ngram_size)} end)
       end)
     |>
       Enum.map(&Task.await/1)
+  end
 
-    for {name1, ngrams1} <- ngrams_for_strings,
-                  {name2, ngrams2} <- ngrams_for_strings,
-                  name1 < name2 do
+  def score_cross_product(lists_of_ngrams) do
+    for {name1, ngrams1} <- lists_of_ngrams,
+        {name2, ngrams2} <- lists_of_ngrams,
+        name1 < name2
+    do
       { name1, name2, jaccard(ngrams1, ngrams2) }
     end
   end
